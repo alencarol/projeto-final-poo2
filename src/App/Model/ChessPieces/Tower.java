de39f599e422c82bb04b3e84c6b0e38df3312238
alencarol <alencarolina25@gmail.com>
2022-06-20 19:16:08 -0300

    Implementação da validação dos movimentos do rei, da rainha e da torre

--- src/App/Model/ChessPieces/Tower.java
+++ src/App/Model/ChessPieces/Tower.java
@@ -9,6 +9,19 @@ public class Tower extends ChessPiece {
 
     @Override
     public boolean ValidMovement(int rowDestination, int colunmDestination) {
-        return true;
+        //linha e coluna atual:
+        int rowOrigin = this.getRow();
+        int colunmOrigin =  this.getColumn();
+
+        ChessPiece piece = this.getChessboard().getChessPiece( rowDestination,colunmDestination); //pega a peça que está na posição de destino
+        if (piece != null  && piece.getColor().equals(this.getColor())) { // retorna falso se tiver uma peça de mesma cor nesse posição
+            return  false;
+        }
+        //Validação do movimento da torre. Nota: a torre anda em vertical ou horizontal
+        if (((rowDestination == rowOrigin) && (colunmDestination != colunmOrigin)) || ((rowDestination != rowOrigin) && (colunmDestination == colunmOrigin))) {
+            return true;
+        }
+
+        return false;
     }
 }
