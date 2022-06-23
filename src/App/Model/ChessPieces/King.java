e999839c6e3b2e3811d487cdd1f251c19980e6ba
lucas <lucascplusmart@gmail.com>
2022-06-22 22:09:25 -0300

    [XDZ-00] implementado a rotina de tempo

--- src/App/Model/ChessPieces/King.java
+++ src/App/Model/ChessPieces/King.java
@@ -9,8 +9,22 @@ public class King extends ChessPiece {
 
     @Override
     public boolean ValidMovement(int rowDestination, int colunmDestination) {
+        //linha e coluna de destino:
+        int rowOrigin = this.getRow();
+        int colunmOrigin =  this.getColumn();
 
-        // Todo implentar o movimento do rei
+        ChessPiece piece = this.getChessboard().getChessPiece( rowDestination,colunmDestination); //pega a peça que está na posição de destino
+        if (piece != null  && piece.getColor().equals(this.getColor())) { // retorna falso se tiver uma peça de mesma cor nesse posição
+            return  false;
+        }
+        //validando movimento do rei. Nota: o rei anda em quadrado 3 X 3
+        if ( ((rowDestination == rowOrigin+1) || (rowDestination == rowOrigin-1)) &&
+                ((colunmDestination == colunmOrigin) || (colunmDestination == colunmOrigin+1) || (colunmDestination == colunmOrigin-1)) ){
+            return true;
+        }else if ( (rowDestination == rowOrigin) && ((colunmDestination == colunmOrigin+1) || (colunmDestination == colunmOrigin-1)) ){
+            return true;
+        }
+
         return false;
     }
 }
--- src/App/Model/ChessPieces/Queen.java
+++ src/App/Model/ChessPieces/Queen.java
@@ -9,6 +9,22 @@ public class Queen extends ChessPiece {
 
     @Override
     public boolean ValidMovement(int rowDestination, int colunmDestination) {
+        //linha e coluna de destino:
+        int rowOrigin = this.getRow();
+        int colunmOrigin =  this.getColumn();
+
+        ChessPiece piece = this.getChessboard().getChessPiece( rowDestination,colunmDestination); //pega a peça que está na posição de destino
+        if (piece != null  && piece.getColor().equals(this.getColor())) { // retorna falso se tiver uma peça de mesma cor nesse posição
+            return  false;
+        }
+        //validando movimento da rainha. Nota: a rainha anda na vertical, na horizontal e na diagonal
+        if ( ( colunmDestination == colunmOrigin+1 || colunmDestination == colunmOrigin -1 ) && (rowDestination == rowOrigin+2 || rowDestination == rowOrigin-2)){
+            return  false;
+        }
+        if ( (colunmDestination == colunmOrigin+2  ||  colunmDestination == colunmOrigin -2) && (rowDestination == rowOrigin+1 || rowDestination == rowOrigin-1)){
+            return  false;
+        }
+
         return true;
     }
 }
